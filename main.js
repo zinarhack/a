04947173b8a285ce087dadb2e6d0862374c7b18d
zinarhack <zinarhack@gmail.com>
2025-05-26 21:47:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const WEBHOOK = 'https://discord.com/api/webhooks/ваш_вебхук';
+const WEBHOOK = 'https://discord.com/api/webhooks/1368583992491642940/8y6EpuqxANKYwIWNgEPlD0th1jf4uD1CeLCwBkvkKdABE_U_e-h-TLZyvQazN1sxeznK';
 
 export default async (req, res) => {
     if (req.method !== 'POST') {
